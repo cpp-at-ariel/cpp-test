a80909e87d24d4c6bd20e6174494e04899dc5586
Erel Segal-Halevi <erelsgl@gmail.com>
2020-03-06 14:20:09 +0200

    grade hack

--- Test.cpp
+++ Test.cpp
@@ -18,7 +18,6 @@ using namespace std;
 
 
 TEST_CASE("Test replacement of p and b") {
-    cout << "Grade: 98" << endl;
     string text = "xxx happy yyy";
     CHECK(find(text, "happy") == string("happy"));
     CHECK(find(text, "habby") == string("happy"));
